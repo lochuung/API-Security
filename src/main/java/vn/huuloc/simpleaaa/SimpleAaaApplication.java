99b48abc7a50ff494dd92d308f2188016114d237
Nguyen Huu Loc <locn562836@gmail.com>
2024-12-27 19:36:05 +0700

    initialize Simple-AAA project with basic configuration and structure

---
+++ src/main/java/vn/huuloc/simpleaaa/SimpleAaaApplication.java
@@ -0,0 +1,13 @@
+package vn.huuloc.simpleaaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleAaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleAaaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/huuloc/simpleaaa/SimpleAaaApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.huuloc.simpleaaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleAaaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
